b532458f5398c6bc2d477eaf50fda53dcb48f8fd
ekelemene <ksuxaskrip2007@gmail.com>
2025-05-30 12:30:30 +0300

    Add files via upload

---
+++ 41zadanie/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41zadanie
+{
+    class Point<T>
+    {
+        private T x;
+        private T y;
+        public T X { get => x; set => x = value; }
+        public T Y { get => y; set => y = value; }
+
+        public Point(T x,T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void Output()
+        {
+            Console.WriteLine($"x: {X}");
+            Console.WriteLine($"y: {Y}");
+        }
+
+    }
+}
---
+++ 41zadanie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> p1 = new Point<int>(10,20);
+            Point<uint> p2 = new Point<uint>(100u, 200u);
+            Point<double> p3 = new Point<double>(1.2, 3.5);
+
+            p1.Output();
+            Console.WriteLine();
+            p2.Output();
+            Console.WriteLine();
+            p3.Output();
+            Console.Read();
+
+        }
+    }
+}
