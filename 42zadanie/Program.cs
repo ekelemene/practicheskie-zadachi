e71f13274c195c7a8e42a7f7f435000ca86e84f9
ekelemene <ksuxaskrip2007@gmail.com>
2025-05-30 12:50:34 +0300

    Add files via upload

---
+++ 42zadanie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle<int> triangle = new Triangle<int>(3, 4, 5);
+            triangle.Print();
+            Console.WriteLine($"Площадь: {triangle.Area():F2}");
+
+            Triangle<double> triangle2 = new Triangle<double>(1, 2, 10); // невалидный треугольник
+            triangle2.Print();
+            Console.WriteLine($"Площадь: {triangle2.Area():F2}"); // будет NaN
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 42zadanie/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42zadanie
+{
+    /// <summary>
+    /// обобщенный класс треугольник
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Triangle<T>
+    {
+        /// <summary>
+        /// поля
+        /// </summary>
+        T a;
+        T b;
+        T c;
+        /// <summary>
+        /// конструктор с 3 параметрами 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public Triangle(T a, T b, T c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+        /// <summary>
+        /// свойтсва с доп логикой
+        /// </summary>
+        public T A { get { return a; } set { a = value; } }
+        public T B { get { return b; } set { b = value; } }
+        public T C { get { return c; } set { c = value; } }
+        /// <summary>
+        /// метод для вывода информации о треуголльнике
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"A: {A}");
+            Console.WriteLine($"B: {B}");
+            Console.WriteLine($"C: {C}");
+        }
+        /// <summary>
+        /// метод для нахождения площади треугольника
+        /// </summary>
+        /// <returns></returns>
+        public double Area() 
+        {
+            double aDouble = Convert.ToDouble(A);
+            double bDouble = Convert.ToDouble(B);
+            double cDouble = Convert.ToDouble(C);
+
+            double s = (aDouble + bDouble + cDouble) / 2;
+            return Math.Sqrt(s * (s - aDouble) * (s - bDouble) * (s - cDouble));
+        }
+
+
+
+    }
+}
